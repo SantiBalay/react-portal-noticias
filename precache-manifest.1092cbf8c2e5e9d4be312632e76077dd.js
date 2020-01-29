de963d517f40dd19b048ea29480372fc4abc5ce7
Santiago <santiagobalay1997@hotmail.com>
2020-01-29 15:14:48 -0300

    Updates

---
+++ precache-manifest.1092cbf8c2e5e9d4be312632e76077dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1e274040de38ebe6eddd2905e807ab",
+    "url": "/react-portal-noticias/index.html"
+  },
+  {
+    "revision": "a0ddb278c3c19553e9ec",
+    "url": "/react-portal-noticias/static/css/main.1c3387ec.chunk.css"
+  },
+  {
+    "revision": "da9995d4cbb893a2973b",
+    "url": "/react-portal-noticias/static/js/2.a4e69297.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-portal-noticias/static/js/2.a4e69297.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0ddb278c3c19553e9ec",
+    "url": "/react-portal-noticias/static/js/main.65ef630c.chunk.js"
+  },
+  {
+    "revision": "f47b08f28a71c3bf882c",
+    "url": "/react-portal-noticias/static/js/runtime-main.1bf9d68b.js"
+  }
+]);
